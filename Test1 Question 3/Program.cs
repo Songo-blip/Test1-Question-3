bb08d65f17d2672836c2a84e20c816bf9a355706
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-11-16 13:38:29 +0200

    Add project files.

---
+++ Test1 Question 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Numbers
+{
+    class Words
+    {
+
+        private static string CapitalizeFirstWord(string sentence)
+
+        {
+            
+        }
+      
+
+        static void Main(string[] args)
+        {
+            string inputString = “I hope you had a good weekend ?”;
+
+            string capitalString = Words.CapitalizeFirstWord(inputString);
+
+            Console.WriteLine(capitalString);
+        }
+    }
+
+}
