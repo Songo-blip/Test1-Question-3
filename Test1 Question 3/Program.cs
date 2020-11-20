3341c4b624fcc7f3888ff70987d7772ddac3383f
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-11-20 12:05:14 +0200

    update

--- Test1 Question 3/Program.cs
+++ Test1 Question 3/Program.cs
@@ -6,16 +6,39 @@ namespace Numbers
     class Words
     {
 
-        private static string CapitalizeFirstWord(string sentence)
+        public static string CapitalizeFirstWord(string sentence)
+          
+            {
+                
+                sentence = sentence.ToLower();
+                char[] array = sentence.ToCharArray();
+             
+                if (array.Length >= 1)
+                {
+                    if (char.IsLower(array[0]))
+                    {
+                        array[0] = char.ToUpper(array[0]);
+                    }
+                }
+               
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if (array[i - 1] == ' ')
+                    {
+                        if (char.IsLower(array[i]))
+                        {
+                            array[i] = char.ToUpper(array[i]);
+                        }
+                    }
+                }
+                return new string(array);
+            }
+        
+   
 
+     static void Main(string[] args)
         {
-            string.toupper
-        }
-      
-
-        static void Main(string[] args)
-        {
-            string inputString = “I hope you had a good weekend ?”;
+            string inputString = ("I hope you had a good weekend?");
 
             string capitalString = Words.CapitalizeFirstWord(inputString);
 
